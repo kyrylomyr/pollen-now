0a4e39e01eba4f9fcc6cc1b41e32e76b9bd798a8
kyrylomyr <1252396+kyrylomyr@users.noreply.github.com>
2022-06-20 13:06:11 +0200

    Fix typos

--- src/components/api-key-explanation.tsx
+++ src/components/api-key-explanation.tsx
@@ -4,14 +4,14 @@ const ApiKeyExplanation: React.FC = () => {
   return (
     <Stack spacing={1}>
       <Typography>
-        Welcome to the application that you will love to use to quickly get the grass polen forecast!
+        Welcome to the application that you will love to use to quickly get a grass polen forecast!
       </Typography>
       <Typography>
         This is a free{' '}
         <Link href="https://github.com/kyrylomyr/pollen-now" underline="none" target="_blank" rel="noreferrer">
           open-source
         </Link>{' '}
-        project withouts ads that does not have a server and works entirely in your browser.
+        project without ads that does not have a server and works entirely in your browser.
       </Typography>
       <Typography>
         A forecast is requested directly from the{' '}
@@ -23,10 +23,10 @@ const ApiKeyExplanation: React.FC = () => {
       <Typography>
         Please,{' '}
         <Link href="https://www.weatherbit.io/account/create" underline="none" target="_blank" rel="noreferrer">
-          create a free Weatherbit account
+          create a personal free Weatherbit account
         </Link>{' '}
-        and provide your personal API Key here. It will be saved in the local storage of your browser and used only for
-        the forecast requests.
+        and provide your API Key here. It will be saved in the local storage of your browser and used only for the
+        forecast requests.
       </Typography>
     </Stack>
   );
