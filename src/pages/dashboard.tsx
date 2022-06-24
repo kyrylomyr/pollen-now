f8df5f516eb06e1ce3d15e1cb0a6c6dd52628873
kyrylomyr <1252396+kyrylomyr@users.noreply.github.com>
2022-06-24 22:46:04 +0200

    Add logic to dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,29 @@
-import { Stack } from '@mui/material';
+import { useContext, useEffect, useState } from 'react';
+
+import { LocationContext } from '../contexts/location-context';
+import { Location } from '../contexts/types/location';
+// import useForecast from '../hooks/use-forecast';
+
+const updateFrequencyHours = 3;
 
 const Dashboard: React.FC = () => {
-  return <Stack />;
+  const locationCtx = useContext(LocationContext);
+  // const { getForecasts } = useForecast();
+
+  const [content, setContent] = useState<Location[]>(locationCtx.locations);
+
+  useEffect(() => {
+    if ((Date.now() - locationCtx.updatedAt) / 36e5 < updateFrequencyHours) {
+      return;
+    }
+
+    // const locationKeys = locationCtx.locations.map((loc) => loc.locationKey);
+    // getForecasts(locationKeys);
+
+    setContent([]);
+  }, []);
+
+  return <pre>{JSON.stringify(content, null, 2)}</pre>;
 };
 
 export default Dashboard;
